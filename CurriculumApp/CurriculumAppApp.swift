ab868fd0a41b16f5f407f4cb85d860623dd53673
ottavianellisim <simone.ottavianelli9@gmail.com>
2022-03-29 18:43:15 +0200

    Initial Commit

---
+++ CurriculumApp/CurriculumAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurriculumAppApp.swift
+//  CurriculumApp
+//
+//  Created by Simone Ottavianelli on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CurriculumAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
